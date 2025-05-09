2d5e33bf9e567f799eec32843407d6c1aea0dcd5
lukebalkema <144456031+lukebalkema@users.noreply.github.com>
2025-05-09 17:12:07 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Universal_Metal_Extractor
+An extractor that can be placed without metal spots
---
+++ ui/mods/com.pa.universal-extractor-server/shared_build.js
@@ -0,0 +1,6 @@
+var newBuild = {
+    "/pa/units/land/universal_metal_extractor/universal_metal_extractor.json": ["utility", 6,{ row: 1, column: 0 }],
+    }
+    if (Build && Build.HotkeyModel && Build.HotkeyModel.SpecIdToGridMap) {
+    _.extend(Build.HotkeyModel.SpecIdToGridMap, newBuild);
+    }
