ba27bb9d3ef9efe446404e7d7ea52ce8cbc0700b
lukebalkema <144456031+lukebalkema@users.noreply.github.com>
2025-05-10 17:47:18 -0400

    Going up now

--- ui/mods/com.pa.universal-extractor-server/shared_build.js
+++ ui/mods/com.pa.universal-extractor-server/shared_build.js
@@ -1,5 +1,5 @@
 var newBuild = {
-    "/pa/units/universal/universal_metal_extractor/universal_metal_extractor.json": ["utility", 6,{ row: 1, column: 0, titans: true }],
+    "/pa/units/universal/universal_metal_extractor/universal_metal_extractor.json": ["utility", 6,{ row: 1, column: 0 }],
     }
     if (Build && Build.HotkeyModel && Build.HotkeyModel.SpecIdToGridMap) {
     _.extend(Build.HotkeyModel.SpecIdToGridMap, newBuild);
