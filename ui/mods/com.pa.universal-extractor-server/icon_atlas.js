aa19c258d319c76e8485abb6485bb513186f2363
lukebalkema <144456031+lukebalkema@users.noreply.github.com>
2025-05-27 16:06:03 -0400

    Update

--- ui/mods/com.pa.universal-extractor-server/icon_atlas.js
+++ ui/mods/com.pa.universal-extractor-server/icon_atlas.js
@@ -1,3 +1,2 @@
-var paeiouIcons = ["universal_metal_extractor",
-];
+var paeiouIcons = ["universal_metal_extractor","universal_metal_extractor_adv"];
 model.strategicIcons(model.strategicIcons().concat(paeiouIcons));
--- ui/mods/com.pa.universal-extractor-server/shared_build.js
+++ ui/mods/com.pa.universal-extractor-server/shared_build.js
@@ -1,5 +1,6 @@
 var newBuild = {
     "/pa/units/universal/universal_metal_extractor/universal_metal_extractor.json": ["utility", 6,{ row: 1, column: 0 }],
+    "/pa/units/universal/universal_metal_extractor_adv/universal_metal_extractor_adv.json": ["combat", 1, {row: 0, column: 1}],
     }
     if (Build && Build.HotkeyModel && Build.HotkeyModel.SpecIdToGridMap) {
     _.extend(Build.HotkeyModel.SpecIdToGridMap, newBuild);
